1317a76a6c09485f9ae1bb3b44bcce3b98d2e2fe
Mayank <mayank@mithyagames.com>
2024-04-11 08:31:43 +0530

    feat(audio): add music and SFX files together with AudioDB

---
+++ Assets/Scripts/ScriptableObjects/AudioDB.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ludo.ScriptableObjects
+{
+    [Serializable]
+    public enum AudioName
+    {
+        TURN,
+        ROLL,
+        MOVE,
+        DICE,
+        KILL,
+        GOAL,
+        TAP,
+        GOTI_TAP
+    }
+
+    [Serializable]
+    public class Audio
+    {
+        public AudioName Name;
+        public AudioClip Clip;
+        public float Volume;
+    }
+
+    [CreateAssetMenu(menuName = "ludo/AudioDB")]
+    public class AudioDB : ScriptableObject
+    {
+        public List<Audio> audios;
+    }
+}
